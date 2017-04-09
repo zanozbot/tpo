feb0e7a5db682777f06878bc4ec89793f7c536ff
TadejVatovani <vatko1995@gmail.com>
2017-04-09 23:40:36 +0200

    date picker fixed

---
+++ resources/views/includes/nalog.blade.php
@@ -0,0 +1,67 @@
+<div class="container">
+	<div class="panel panel-default">			
+	  <div class="panel-heading">
+		<h3 class="panel-title">Delovni nalog</h3>
+	  </div>
+	  <div class="panel-body">
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-0">
+				<div class="panel panel-default">			
+				  <div class="panel-heading">
+					<h3 class="panel-title">Izvajalec</h3>
+				  </div>
+				  <div class="panel-body">
+						<form role="form" >  
+							  <div class="form-group">
+								<label class="label label-primary">Šifra uslužbenca</label>
+								<input type="number" name="sifrausluzbenca" class="form-control input-sm" min="1" placeholder="12345" required>
+							  </div>
+							  <div class="form-group">
+								<label class="label label-primary">Šifra izvajalca zdravstvene dejavnosti</label>
+								<input type="number" name="sifraizvajalca" class="form-control input-sm" min="1" placeholder="12345" required>
+							  </div>
+						</form>
+				  </div>
+				</div>
+				<div class="panel panel-default">			
+				  <div class="panel-heading">
+					<h3 class="panel-title">Zavarovana oseba</h3>
+				  </div>
+				  <div class="panel-body">
+						<form role="form" >  
+							  <div class="form-group">
+								<label class="label label-primary">Šifra uslužbenca</label>
+								<input type="number" name="sifrausluzbenca" class="form-control input-sm" min="1" placeholder="12345" required>
+							  </div>
+							  <div class="form-group">
+								<label class="label label-primary">Šifra izvajalca zdravstvene dejavnosti</label>
+								<input type="number" name="sifraizvajalca" class="form-control input-sm" min="1" placeholder="12345" required>
+							  </div>
+						</form>
+				  </div>
+				</div>				
+			</div>
+			<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-6">
+				<div class="panel panel-default">			
+					  <div class="panel-heading">
+						<h3 class="panel-title">Zdravnik</h3>
+					  </div>
+					  <div class="panel-body">
+							<form role="form" >  
+								  <div class="form-group">
+									<label class="label label-primary">Šifra uslužbenca</label>
+									<input type="number" name="sifrausluzbenca" class="form-control input-sm" min="1" placeholder="12345" required>
+								  </div>
+								  <div class="form-group">
+									<label class="label label-primary">Šifra izvajalca zdravstvene dejavnosti</label>
+									<input type="number" name="sifraizvajalca" class="form-control input-sm" min="1" placeholder="12345" required>
+								  </div>
+							</form>
+					  </div>
+				</div>
+			</div>
+	  </div>
+	</div>
+	</div>
+</div>
+
