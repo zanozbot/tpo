0062573aeb912b8781fcb5eb889426e8858ccc71
TadejVatovani <vatko1995@gmail.com>
2017-04-10 23:32:55 +0200

    Končan vmesnik za #6 in

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="container">
-<div style="text-align: center" class="navbar navbar-fixed-bottom navbar-default">© Copyright 2017 We Made This</div>
+<!--<div style="text-align: center" class="navbar navbar-fixed-bottom navbar-default">© Copyright 2017 We Made This</div>-->
 </div>
--- resources/views/includes/nalog.blade.php
+++ resources/views/includes/nalog.blade.php
@@ -1,111 +1,225 @@
-<div class="container">
-	<div class="panel panel-default">			
-	  <div class="panel-heading">
-		<h3 class="panel-title">Delovni nalog</h3>
-	  </div>
-	  <div class="panel-body">
-		<div class="row">
-		<form role="form">  
+<div class="panel panel-default">			
+  <div class="panel-heading">
+	<h3 class="panel-title">Delovni nalog</h3>
+  </div>
+  <div class="panel-body">
+	<div class="row">
 		<div class="col-xs-12 col-sm-8 col-md-6">
 			<div class="panel panel-default">			
 			  <div class="panel-heading">
-				<h3 class="panel-title">Izvajalec</h3>
+				<h3 class="panel-title">1 - Izvajalec</h3>
 			  </div>
 			  <div class="panel-body">	
 				  <div class="row">
-				    <div class="col-lg-6">
+					<div class="col-lg-6">
 					  <div class="form-group">
 						<label class="label label-primary">Številka izvajalca</label>
-						<input type="number" name="sifraIzvajalca" class="form-control input-sm" min="1" placeholder="12345" required>
+						<div class="form-control nalog" name="stIzvajalca">Value</div>
 					  </div>
 					</div>
 					<div class="col-lg-6">
 					  <div class="form-group">
 						<label class="label label-primary">Šifra zdravstvene dejavnosti</label>
-						<input type="number" name="sifraDejavnosti" class="form-control input-sm" min="1" placeholder="12345" required>
+						<div class="form-control nalog" name="sifraZdrDejavnosti">Value</div>
 					  </div>
 					</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Naziv izvajalca</label>
-					<input type="text" name="nazivIzvajalca" class="form-control input-sm" required>
+					<div class="form-control nalog" name="nazivIzvajalca">Value</div>
 				  </div>
 			  </div>
 			</div>
 			<div class="panel panel-default">			
 			  <div class="panel-heading">
-				<h3 class="panel-title">Zavarovana oseba</h3>
+				<h3 class="panel-title">3 - Zavarovana oseba</h3>
 			  </div>
 			  <div class="panel-body">
 				  <div class="form-group">
 					<label class="label label-primary">Številka zavarovane osebe</label>
-					<input type="text" name="stZavOsebe" class="form-control input-sm" required>
+					<div class="form-control nalog" name="stZavOsebe">Value</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Datum rojstva</label>
-					<div class="datepicker input-group date" data-provide="datepicker">
-						<input type="text" class="form-control" placeholder="dd/mm/yyyy" name="datumRojstva">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-th"></span>
-						</div>
-					</div>
+					<div class="form-control nalog" name="rojstvoZavOsebe">Value</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Priimek</label>
-					<input type="text" name="priimekZavOsebe" class="form-control input-sm" required>
+					<div class="form-control nalog" name="priimekZavOsebe">Value</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Ime</label>
-					<input type="text" name="imeZavOsebe" class="form-control input-sm" required>
+					<div class="form-control nalog" name="imeZavOsebe">Value</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Ulica, hišna številka</label>
-					<input type="text" name="imeZavOsebe" class="form-control input-sm" required>
+					<div class="form-control nalog" name="ulicaZavOsebe">Value</div>
 				  </div>
 				  <div class="row">
-				    <div class="col-lg-6">
+					<div class="col-lg-6">
 					  <div class="form-group">
 						<label class="label label-primary">Poštna številka</label>
-						<input type="text" name="imeZavOsebe" class="form-control input-sm" required>
+						<div class="form-control nalog" name="postaZavOsebe">Value</div>
 					  </div>
 					</div>
 					<div class="col-lg-6">
 					  <div class="form-group">
 						<label class="label label-primary">Kraj</label>
-						<input type="text" name="imeZavOsebe" class="form-control input-sm" required>
+						<div class="form-control nalog" name="krajZavOsebe">Value</div>
 					  </div>
 					</div>
 				  </div>	 
 				  <div class="form-group">
 					<label class="label label-primary">Email</label>
-					<input type="email" name="email" class="form-control input-sm" placeholder="email@email.si">
+					<div class="form-control nalog" name="emailZavOsebe">Value</div>
 				  </div>
 				  <div class="form-group">
 					<label class="label label-primary">Telefonska številka</label>
-					<input type="text" pattern="[0-9]{9}" name="tel_stevilka" class="form-control input-sm" maxlength="9" placeholder="031234567">
+					<div class="form-control nalog" name="telZavOsebe">Value</div>
 				  </div>			  
 			  </div>
-			</div>				
+			</div>	
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">8 - Tuji zavarovanec</h3>
+			  </div>
+			  <div class="panel-body">					  
+					  <div class="form-group">
+						<label class="label label-primary">Šifra države</label>
+						<div class="form-control nalog" name="sifraDrzave">Value</div>
+					  </div>
+				</div>
+			</div>
 		</div>
 		<div class="col-xs-12 col-sm-8 col-md-6">
 			<div class="panel panel-default">			
-				  <div class="panel-heading">
-					<h3 class="panel-title">Zdravnik</h3>
+			  <div class="panel-heading">
+				<h3 class="panel-title">2 - Zdravnik</h3>
+			  </div>
+			  <div class="panel-body">
+				  <div class="form-group">
+					<label class="label label-primary">Številka zdravnika</label>
+					<div class="form-control nalog" name="stZdravnika">Value</div>
 				  </div>
-				  <div class="panel-body">
-						  <div class="form-group">
-							<label class="label label-primary">Vrsta zdravnika</label>
-							<label class="radio-inline"><input type="radio" class="radio-inline" name="vrstaZdravnik">Osebni</label>
-							<label class="radio-inline"><input type="radio" class="radio-inline" name="vrstaZdravnik">Napotni</label>
-							<label class="radio-inline"><input type="radio" class="radio-inline" name="vrstaZdravnik">NMP</label>
-							<label class="radio-inline"><input type="radio" class="radio-inline" name="vrstaZdravnik">Nadomestni</label>
-						  </div>
+				  <div class="form-group" >
+					<label class="label label-primary">Vrsta zdravnika</label>
+					<label class="checkbox-inline"><input type="checkbox" name="vrstaZdravnik" disabled>Osebni</label>
+					<label class="checkbox-inline"><input type="checkbox" name="vrstaZdravnik" disabled>Napotni</label>
+					<label class="checkbox-inline"><input type="checkbox" name="vrstaZdravnik" disabled>NMP</label>
+					<label class="checkbox-inline"><input type="checkbox" name="vrstaZdravnik" disabled>Nadomestni</label>
 				  </div>
+				  
+				  <div class="form-group">
+					<label class="label label-primary">Imenski žig</label>
+					<div class="form-control nalog" name="imenskiZig">Value</div>
+				  </div>
+			  </div>
+			</div>
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">4 - Napotnica</h3>
+			  </div>
+			  <div class="panel-body">					  
+				  <div class="form-group">
+					<label class="label label-primary">Številka napotnice</label>
+					<div class="form-control nalog" name="stNapotnice">Value</div>
+				  </div>
+				  <div class="form-group">
+					<label class="label label-primary">Številka zdravnika</label>
+					<div class="form-control nalog" name="stZdravnikaNapotnice">Value</div>
+				  </div>
+				</div>
+			</div>
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">5 - Veljavnost naloga</h3>
+			  </div>
+			  <div class="panel-body">					  
+				  <div class="form-group">
+					<div class="form-control nalog" name="veljavnostNaloga">Value</div>
+				  </div>
+				</div>
+			</div>
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">6 - Vrsta storitve</h3>
+			  </div>
+			  <div class="panel-body">					  
+				  <div class="form-group">
+					<div class="form-control nalog" name="vrstaStoritve">Value</div>
+				  </div>
+				</div>
+			</div>
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">7 - Razlog obravnave</h3>
+			  </div>
+			  <div class="panel-body">					  
+				  <div class="form-group">
+					<div class="form-control nalog" name="razlogObravnave">Value</div>
+				  </div>
+			  </div>
 			</div>
 		</div>
-		</form>
-	  </div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">Napoten k izvajalcu</h3>
+			  </div>
+			  <div class="panel-body">					  
+				  <div class="form-group">
+					<label class="label label-primary">Naziv in naslov</label>
+					<div class="form-control nalog" name="izvajalec">Value</div>
+				  </div>
+			  </div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">Podatki o bolezni(vzrok za napotitev)</h3>
+			  </div>
+			  <div class="panel-body">					  
+				<div class="form-control nalog" name="vzrok">Test kako zgleda če imamo  veliko texta za opis bolezni halalalalalalalalalalalalalalalalalalalalalalalalalalalala
+				lalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalala</div>
+			  </div>
+			</div>
+		</div>
 	</div>
-	</div>
+  </div>
 </div>
+<div class="panel panel-default">			
+  <div class="panel-heading">
+	<h3 class="panel-title">Naročene storitve</h3>
+  </div>
+  <div class="panel-body">
+	  <table class="table table-bordered">
+		<thead>
+		  <tr>
+			<th>Zap. št.</th>
+			<th>Opis</th>
+			<th>Število</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+			<td>Value</td>
+			<td>Value</td>
+			<td>Value</td>
+		  </tr>
+		  <tr>
+			<td>Value</td>
+			<td>Value</td>
+			<td>Value</td>
+		  </tr>
+		  <tr>
+			<td>Value</td>
+			<td>Value</td>
+			<td>Value</td>
+		  </tr>
+		</tbody>
+	  </table>
+  </div>
+</div>
+
 
