a7ad8b8fb142c83e7135c6ccfa8bd0a33e26b0ab
TadejVatovani <vatko1995@gmail.com>
2017-04-11 23:55:49 +0200

    dodan UI za filter zgodba #7

--- resources/views/includes/seznamnalog.blade.php
+++ resources/views/includes/seznamnalog.blade.php
@@ -1,10 +1,87 @@
 <div class="container">
-
 	<div class="panel panel-default">			
 	  <div class="panel-heading">
 		<h3 class="panel-title">Seznam Nalogov</h3>
 	  </div>
 	  <div class="panel-body">
+		  <div class="panel panel-default">			
+			  <div class="panel-heading">
+				<h3 class="panel-title">Filter</h3>
+			  </div>
+			  <div class="panel-body"> 
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-6">
+						<div class="form-group">
+						  <label class="label label-primary">Izdajalec</label>
+						  <input type="text" class="form-control input-sm" name="izdajalec" ></input>	
+						</div>
+						<div class="form-group">
+						  <label class="label label-primary">Pacient</label>
+						  <input type="text" class="form-control input-sm" name="pacient" ></input>
+						</div>
+						<div class="form-group">  
+						  <label class="label label-primary">Zadolžena patronažna sestra</label>
+						  <input type="text" class="form-control input-sm" name="zadolzenaSestra" ></input>
+						</div>
+						<div class="form-group">
+						  <label class="label label-primary">Nadomestna patronažna sestra</label>
+						  <input type="text" class="form-control input-sm" name="nadomestnaSestra" ></input>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-6">
+						<form>
+							<div class="form-group">
+							  <label class="label label-primary">Vrsta obiska</label>
+							  <select class="form-control input-sm" name="obisk">
+								<option>-</option>
+								<option>Obisk nosečnice</option>
+								<option>Obisk otročnice</option>
+								<option>Preventiva starostnika</option>	
+								<option>Aplikacija injekcij</option>
+								<option>Odvzem krvi</option>
+								<option>Kontrola zdravstvenega stanja</option>	
+							  </select>
+							</div>
+						</form>
+						<div class="form-group" name="odDatumDiv">
+						  <label class="label label-primary">Od datuma</label>
+						  <div class="datepicker input-group date" data-provide="datepicker">
+							<input type="text" class="form-control" placeholder="dd/mm/yyyy" name="odDatum">
+							<div class="input-group-addon">
+							  <span class="glyphicon glyphicon-th"></span>
+							</div>
+						  </div>
+						  <script>
+							$('.datepicker').datepicker({
+								format: "dd/mm/yyyy",
+								clearBtn: true,
+								autoclose: true,
+								todayHighlight: true
+							});
+						  </script>
+						</div>	
+						<div class="form-group" name="doDatumDiv">
+						  <label class="label label-primary">Do datuma</label>
+						  <div class="datepicker input-group date" data-provide="datepicker">
+							<input type="text" class="form-control" placeholder="dd/mm/yyyy" name="doDatum">
+							<div class="input-group-addon">
+							  <span class="glyphicon glyphicon-th"></span>
+							</div>
+						  </div>
+						  <script>
+							$('.datepicker').datepicker({
+								format: "dd/mm/yyyy",
+								clearBtn: true,
+								autoclose: true,
+								todayHighlight: true
+							});
+						  </script>
+						</div>
+					</div>
+				</div>
+			  </div>
+		  </div>
+			  
 		  <table class="table ">
 			<thead>
 			  <tr>
--- resources/views/pages/nalog.blade.php
+++ resources/views/pages/nalog.blade.php
@@ -205,4 +205,6 @@
     </div>
   </div>
 </div>
+
+@include('includes.seznamnalog')
 @stop
