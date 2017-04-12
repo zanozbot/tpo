ca77d7d9aa60d27f70d7ffc9901b779657419795
Žan Ožbot <zan.ozbot@gmail.com>
2017-04-12 15:44:55 +0200

    Merge

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -3,17 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Obisk
+use App\Obisk;
+use App\DelovniNalog;
 
 class PlanController extends Controller
 {
     public function index() {
-        $obiski = Obisk::all();
-
-    	return view('pages.plan', ['obiski' => $obiski]);
+    	$sifraPlan = 0;
+        if (session()->has('sifraPlan')){
+        	$sifraPlan = session('sifraPlan');
+        }
+		$obiski = Obisk::join('delovni_nalog', 'obisk.sifra_dn', '=', 'delovni_nalog.sifra_dn')
+							->where('opravljen', '=', 0)
+							->where('sifra_plan', '!=', $sifraPlan)
+                        	->get(array(
+		                            'sifra_obisk',
+		                            'sifra_plan',
+		                            'sifra_ps',
+		                            'datum_obiska',
+		                            'opravljen',
+		                            'nadomesca',
+		                            'datum_obvezen'
+                        ));
+        $obiskiVPlanu = Obisk::join('delovni_nalog', 'obisk.sifra_dn', '=', 'delovni_nalog.sifra_dn')
+							->where('opravljen', '=', 0)
+							->where('sifra_plan', '=', $sifraPlan)
+                        	->get(array(
+		                            'sifra_obisk',
+		                            'sifra_plan',
+		                            'sifra_ps',
+		                            'datum_obiska',
+		                            'opravljen',
+		                            'nadomesca',
+		                            'datum_obvezen'
+                        ));
+    	return view('pages.plan', ['obiski' => $obiski, 'obiskiVPlanu' => $obiskiVPlanu, 'sifraPlan' => $sifraPlan]);
     }
 
-    public function create(Request $request) {
+    public function dodaj($sifraPlan, $sifraObiska) {
+    	$obisk = Obisk::where('sifra_obisk', '=', $sifraObiska)->get();
+    	$obisk = Obisk::where('sifra_obisk', '=', $sifraObiska)->update(['sifra_plan' => $sifraPlan]);
     	
+    	return redirect()->route('plan')->with('sifraPlan', $sifraPlan);
     }
 }
--- resources/views/pages/plan.blade.php
+++ resources/views/pages/plan.blade.php
@@ -8,21 +8,21 @@
 					<h3 class="panel-title">Neopravljeni obiski</h3>
 				  </div>
 				  <div class="panel-body">
-					<form role="form" >
+					<form role="form" method="post">
 						<!-- seznam neopravljenih obiskov določene MS, vključno z obiski, kjer MS nadomešča -->
-						<div class="list-group-item checkbox">
-							<label><input type="checkbox" value="">Neobvezen obisk 1</label>
-						</div>
-						<div class="list-group-item checkbox">
-							<label><input type="checkbox" value="">Neobvezen obisk 2</label>
-						</div>
-						<div class="list-group-item checkbox">
-							<label><input type="checkbox" value="">Obisk nadomeščanje 1</label>
-						</div>
-						<div class="list-group-item checkbox">
-							<label><input type="checkbox" value="">Obisk nadomeščanje 2</label>
-						</div>
-						<input type="submit" value="Dodaj obiske v plan" class="btn btn-info btn-block">
+						@foreach ($obiski as $obisk)
+							@if ($obisk->datum_obvezen == 1)
+								<div class="list-group-item checkbox">
+									<button type="button" onclick="window.location='{{ url('plan/dodaj') }}/{{$sifraPlan}}/{{$sifra = $obisk->sifra_obisk}}'" class="btn btn-default" name="{{$sifra = $obisk->sifra_obisk}}" disabled>Dodaj</button>
+									<label>{{$obisk->datum_obiska}}</label>
+								</div>
+							@else
+								<div class="list-group-item checkbox">
+									<button type="button" onclick="window.location='{{ url('plan/dodaj') }}/{{$sifraPlan}}/{{$sifra = $obisk->sifra_obisk}}'" class="btn btn-default" name="{{$sifra = $obisk->sifra_obisk}}">Dodaj</button>
+									<label>{{$obisk->datum_obiska}}</label>
+								</div>
+							@endif
+						@endforeach
 					</form>
 				  </div>
 				</div>
@@ -33,42 +33,19 @@
 					<h3 class="panel-title">Planirani obiski</h3>
 				  </div>
 				  <div class="panel-body">
-					<form role="form" >
-						<!-- datepicker -->
-						<div class="datepicker input-group date" data-provide="datepicker">
-							<input type="text" class="form-control" placeholder="dd/mm/yyyy" name="datumPlana">
-							<div class="input-group-addon">
-								<span class="glyphicon glyphicon-th"></span>
-							</div>
-						</div>
-						<script>
-							$('.datepicker').datepicker({
-								format: "dd/mm/yyyy",
-								todayBtn: "linked",
-								clearBtn: true,
-								autoclose: true,
-								todayHighlight: true
-							});
-						</script>
-						<!-- seznam obiskov v planu, neobvezne se da odstraniti, obvezne pa ne -->
+				  	@foreach ($obiskiVPlanu as $obisk)
+				  		@if ($obisk->datum_obvezen == 1)
 						<div class="list-group-item checkbox">
-							<button type="button" class="btn btn-default" disabled>Odstrani</button>
-							<label>Obvezen obisk 1</label>
+							<button type="button" class="btn btn-default" name="{{$sifra = $obisk->sifra_obisk}}" disabled>Odstrani</button>
+							<label>{{$obisk->datum_obiska}}</label>
 						</div>
+						@else
 						<div class="list-group-item checkbox">
-							<button type="button" class="btn btn-default" disabled>Odstrani</button>
-							<label>Obvezen obisk 2</label>
+							<button type="button" class="btn btn-default" name="{{$sifra = $obisk->sifra_obisk}}">Odstrani</button>
+							<label>{{$obisk->datum_obiska}}</label>
 						</div>
-						<div class="list-group-item checkbox">
-							<button type="button" class="btn btn-default" disabled>Odstrani</button>
-							<label>Obvezen obisk 3</label>
-						</div>
-						<div class="list-group-item checkbox">
-							<button type="button" class="btn btn-default">Odstrani</button>
-							<label>Neobvezen obisk 3</label>
-						</div>
-						<input type="submit" value="Shrani plan" class="btn btn-info btn-block">
-					</form>
+						@endif
+					@endforeach
 				  </div>
 				</div>
 			</div>
