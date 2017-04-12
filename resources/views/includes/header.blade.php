41ab755763dac072a61c0f43dddce2a5b602e763
Dik Medvešček Murovec <dik.med.mur@gmail.com>
2017-04-12 23:24:59 +0200

    Posodobljen header glede na uporabniške vloge

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,26 +11,35 @@
 		</div>
 		<div class="collapse navbar-collapse " id="navbar-collapse-1">
 			<ul class="nav navbar-nav pull-right">
+				@if (Auth::check() && (Auth::user()->sifra_vloga == 2 || Auth::user()->sifra_vloga == 3))
 				<li>
 					<a href="{{route('nalog')}}">Nalog</a>
 				</li>
+				@endif
+				@if (Auth::check() && (Auth::user()->sifra_vloga == 4))
 				<li>
 					<a href="obisk.html">Obisk</a>
 				</li>
+				@endif
+				<!-- Trenutno funkcionalnost ni podprta
 				<li>
 					<a href="material.html">Material</a>
-				</li>
+				</li>-->
+				@if (Auth::check() && (Auth::user()->sifra_vloga == 4))
 				<li>
 					<a href="{{route('datumPlan')}}">Plan</a>
 				</li>
+				@endif
+				@if (Auth::check() && (Auth::user()->sifra_vloga == 1))
 				<li >
 					<a href="{{ url('admin') }}">Admin</a>
 				</li>	
+				@endif
 				@if (Auth::check())
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{Auth::user()->ime}}<span class="caret"></span></a>
 					<ul class="dropdown-menu">
-			            <li><a href="#">Poduporabnik</a></li>
+			            <li><a href="#">Poduporabnik #1</a></li>
 			            <li><a href="{{route('register')}}">Dodaj poduporabnika</a></li>
 			            <li><a href="{{route('odjava')}}">Odjava</a></li>
 			          </ul>
