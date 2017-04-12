592742a3e2f2a603250440ca93dc6bf370941e0b
Žan Ožbot <zan.ozbot@gmail.com>
2017-04-12 15:41:16 +0200

    Revert "Spreminjanje gesla"
    This reverts commit a083caf933993f2ef04bf4ca5b6844a7150b1419.

--- resources/views/pages/newPassword.blade.php
+++ resources/views/pages/newPassword.blade.php
@@ -1,15 +1,6 @@
 @extends('layouts.default')
 @section('content')
 	<div class="container">
-		@if (count($errors) > 0)
-			<div class="alert alert-danger">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 				<div class="panel panel-default">			
@@ -17,9 +8,9 @@
 					<h3 class="panel-title">Sprememba gesla</h3>
 				  </div>
 				  <div class="panel-body">
-						<form role="form" method="post">
+						<form role="form" >
 						  <div class="form-group">
-							<label class="label label-primary">Novo geslo</label>
+							<label class="label label-primary">Geslo</label>
 							<input type="password" pattern="(?=.*\d).{8,}" name="geslo" class="form-control input-sm" placeholder="Geslo" 
 							title="najmanj 8 znakov, vsaj en numeričen" required>
 						  </div>
@@ -29,7 +20,6 @@
 							title="najmanj 8 znakov, vsaj en numeričen" required>
 						  </div>
 						  <input type="submit" value="Spremeni geslo" class="btn btn-info btn-block">
-						  {{ csrf_field() }}
 						</form>
 				  </div>
 				</div>
